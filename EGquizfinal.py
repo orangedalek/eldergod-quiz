6c64a906be8652b707a45083370417294422745c
orangedalek <jasnider@mac.com>
2017-05-24 18:14:29 -0700

    Add files via upload

--- EGquizfinal.py
+++ EGquizfinal.py
@@ -7,28 +7,34 @@ if begin == "begin".lower():
 else:
     print "maybe later!"
 
+#keep prompting user to play
+while begin != "begin".lower():
+    print "Sorry, that's not a valid response. Please enter begin to play."
+    begin = raw_input("""Welcome to the elder god quiz! Type begin to play! """)
+
 kittens = {"prompt": """Pick a cuddly kitten!
         a) Steve! Ginger and cuddly!
         b)Sammie! Calico and sassy!
         c) Tiger! Grey tabby and fluffy!
-        d)Minerva! Tortie and wise!""", "a": 25, "b":50, "c": 75, "d": 100 }
+        d)Minerva! Tortie. Her love is pointy!
+        """, "a": 25, "b":50, "c": 75, "d": 100 }
 
 
-ukfood = {"prompt":""""Pick a British comfort food!
+ukfood = {"prompt":"""Pick a British comfort food!
         a) Curry!
         b)Fry-up
         c)Yorkshire Pudding!
         d)Fish & Chips!
           """, "a": 75, "b": 25, "c": 100, 'd': 50}
 
-ryans = {"prompt": """"Pick a famous Ryan!
+ryans = {"prompt": """Pick a famous Ryan!
         a) Ryan Reynolds!
         b)Ryan Goesling!
         c)Ryan Seacrest!
         d)Ryan Lochte!
           """, "a": 100, "b": 50, "c": 75, "d": 25}
 
-unicorn = {"prompt":""""Pick a unicorn food!
+unicorn = {"prompt":"""Pick a unicorn food!
         a)Unicorn toast!
         b)Unicorn frappuccino!
         c)Unicorn pasta!
@@ -44,7 +50,7 @@ decor = {"prompt": """Choose a style to decorate your room!
         d)Brutalist
         """, "a": 25, "b": 50, "c": 25, "d": 100}
 
-fashion = {"prompt": """Pich a terrible 00s fashion trend!
+fashion = {"prompt": """Pick a terrible 00s fashion trend!
         a)Low rise jeans
         b)Glitter everything
         c)Tinted visor shades
@@ -68,27 +74,34 @@ cumberbatch = {"prompt": """Pick an alternate name for Benedict Cumberbatch!
 
 questions = [kittens, ukfood, ryans, unicorn, decor, fashion, pizza, cumberbatch]
 for question in questions:
-    answer = raw_input(question["prompt"])
-    if answer in question:
-        total_score = question[answer] + total_score
-    else:
-        print "Please pick a,b,c or d"
+    while True:
+        answer = raw_input(question["prompt"])
+        if not answer in question:
+            print "Please pick a, b, c, or d."
+            continue
+        else:
+            total_score = question[answer] + total_score
+            break
+    
+       
 
-if answer <= 200:
+
+
+if total_score <= 200:
     print """Congratulations, you are Cthulu!
-    You love naps and the sea. You’re stoic and easy going most of the time, 
-    but aren’t afraid to devout the soul of anyone who dares to disturb your slumber."""
-elif answer <= 400:
+    You love naps and the sea. You're stoic and easygoing, but aren't 
+    afraid to devour the soul of anyone who dares to disturb your slumber."""
+elif total_score <= 400:
     print """Congratulations, you are Nyogtha, The Thing That Should Not Be!
-    While you used to haunt the red abyss, these days, you prefer to haunt the local Sephora, 
-    where you’ve perfected the smokey eye."""
-elif answer <= 600:
-    print """Congratulations, you are Tsathoggua! 
-    You’ve been around long enough to know what you want,
-    and what you want is Pizza and alone time. Your home is your sanctuary, 
-    and you hardly ever leave, even to terrorize the people of K’n-yan."""
+    While you used to haunt the red abyss, these days you prefer to haunt your 
+    local Sephora, where you've perfected your smokey eye look."""
+elif total_score <= 600:
+    print """Congratulations, you are Tsathoggua!
+    You've been aruund long enough to know what you want, and what you want is pizza
+    and alone time. Your home is your sanctuary, and you rarely leave it, even to 
+    terrorize the good people of K'n-yan."""
 else:
-    print """Congratulations, you are The Worm That Gnaws in the Night! 
-    People lose their minds in terror just from looking at you, but you’re 
-    really a misunderstood party animal who loves dancing, late night burritos, 
+    print """Congratulations, you are The Worm That Gnaws in the Night!
+    People lose their minds in terror just from looking at you, but you"re
+    really just a misunderstood party animal who loves dancing, late night burritos,
     and the eternal chaos that awaits us all upon our deaths."""
